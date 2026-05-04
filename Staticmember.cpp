672c47e7c92b2d5f842bf83038318aec76768646
gigayulianti <yulianti2@gmail.com>
2026-05-04 20:44:49 +0700

    commit 2

--- Staticmember.cpp
+++ Staticmember.cpp
@@ -1,6 +1,8 @@
-include <iostream>
+
+
+#include <iostream>
 #include <string> //menyertakan library string
-us#ing namespace std;
+using namespace std;
 
 class Mahasiswa {
 
@@ -32,4 +34,33 @@ int Mahasiswa::nim = 0;
 //pendefinisial prosedur-prosedur diluar class
 void Mahasiswa::setID() {
     id = ++nim;
+}
+
+void Mahasiswa::printAll(){
+    cout << "ID = " << id << endl;
+    cout << "Nama = " << nama << endl;
+    cout << endl;
+}
+
+int main() {
+    //pembuatan object dan pemberian nilai
+    Mahasiswa mhs1("Sri Dadi");
+    Mahasiswa mhs2("Budi Jatmiko");
+    //memberi nilai pada setNim() untuk merubah nilai NIM
+    Mahasiswa::setNim(9);
+    Mahasiswa mhs3("Andi Janu");
+    Mahasiswa mhs4("Joko Wahono");
+
+    //memanggil prosedur printAll()
+    mhs1.printAll();
+    mhs2.printAll();
+    mhs3.printAll();
+    mhs4.printAll();
+
+    //menampilkan NIM terakhir yang diproses
+    cout << "Akses dari luar object = " << Mahasiswa::getNim() << endl;
+    system("pause");
+
+    return 0;
+    
 }
